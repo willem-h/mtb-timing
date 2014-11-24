9878c76b8b667e4cfd23cfb7b5d6dde75dd019fd
Willem Handreck <whreck1972@live.com>
2014-11-24 22:43:49 +1100

    Try to fix Openshift issue.

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ io.on('connection', function(socket){
 // });
 
 var port = Number(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 5000);       // Openshift OR Heroku OR Local
-var ip = process.env.OPENSHIFT_NODEJS_IP || 127.0.0.1;
+var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 server.listen(port, ip, function(){
     console.log("Listening on "+ port);
 });
